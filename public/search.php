04331d115bcd2a6c489242ec08750cfaec048d8f
Onkar Rai <onkar21.rai@gmail.com>
2017-07-12 06:20:20 +0000

    Implemented a very basic search..only for postal code...will improve it

--- public/search.php
+++ public/search.php
@@ -5,8 +5,16 @@
     // numerically indexed array of places
     $places = [];
 
+    $geo = $_GET["geo"];
+    
     // TODO: search database for places matching $_GET["geo"], store in $places
-
+    
+    //Searches for the postal code only..
+    if (is_numeric($geo))
+    {
+        $places = CS50::query("SELECT * FROM places where postal_code LIKE ?", $geo . "%");   
+    }
+    
     // output places as JSON (pretty-printed for debugging convenience)
     header("Content-type: application/json");
     print(json_encode($places, JSON_PRETTY_PRINT));
